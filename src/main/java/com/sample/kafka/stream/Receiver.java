b3d70bcc29978af67328ad27f4e3e0134ba6d4ba
fahad-rauf <fahad.abdulrauf@gmail.com>
2018-09-24 18:39:53 +0200

    add src

---
+++ src/main/java/com/sample/kafka/stream/Receiver.java
@@ -0,0 +1,24 @@
+package com.sample.kafka.stream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+
+@EnableBinding(Sink.class)
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    @StreamListener(Sink.INPUT)
+    public void handle(
+            @Payload final String message,
+            @Header(KafkaHeaders.OFFSET) final Long offset,
+            @Header(KafkaHeaders.RECEIVED_PARTITION_ID) final Integer partition) {
+
+        LOGGER.info("Receiving test message (partition:{}, offset:{}): {}", partition, offset, message);
+    }
+}
