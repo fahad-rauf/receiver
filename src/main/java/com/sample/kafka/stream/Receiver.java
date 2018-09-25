a487334354178f75ad32d9641061d8f025d7812e
fahad-rauf <fahad.abdulrauf@gmail.com>
2018-09-25 11:41:47 +0200

    add rbac

--- src/main/java/com/sample/kafka/stream/Receiver.java
+++ src/main/java/com/sample/kafka/stream/Receiver.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
-import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.Payload;
